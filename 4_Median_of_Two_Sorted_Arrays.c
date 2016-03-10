0e37a3950a2efe1d66a2a0fd5caf785b0ddad598
louis <345722430@qq.com>
2016-03-10 22:00:24 +0800

    [Q4] update median of two sorted arrays

--- 4_Median_of_Two_Sorted_Arrays.c
+++ 4_Median_of_Two_Sorted_Arrays.c
@@ -11,11 +11,15 @@
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
     int sum = nums1Size+nums2Size;
-    int i, j, k;
+    int i = 0, j = 0, k = 0;
     int nums[sum];
     double median;
 
-    for (i = 0, j = 0, k = 0; i < nums1Size, j < nums2Size;k++) {
+    if (!sum) {
+        return 0;
+    }
+
+    while (i < nums1Size && j < nums2Size) {
         if(*(nums1 + i) <= *(nums2 + j)) {
             nums[k] = *(nums1 + i);
             i++;
@@ -23,11 +27,30 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
             nums[k] = *(nums2 + j);
             j++;
         }
+        k++;
     }
+
+    if (i != nums1Size) {
+        while (i < nums1Size) {
+            nums[k] = *(nums1 + i);
+            i++;
+            k++;
+        }
+    }
+
+    if (j != nums2Size) {
+        while (j < nums2Size) {
+            nums[k] = *(nums2 + j);
+            j++;
+            k++;
+        }
+    }
+
     if (sum % 2) {
         median = nums[sum / 2];
     } else {
-        median = nums[(sum/2) - 1] + nums[sum/2];
+        median = (double)(nums[(sum/2) - 1] + nums[sum/2]) / 2;
     }
+
     return median;
 }
