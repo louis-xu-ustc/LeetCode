766638754edbb13b6fb354a1bf5782b9b0d31b44
louis <345722430@qq.com>
2016-03-22 23:21:39 +0800

    [LeetCode - Q22] init Q22

---
+++ 22_Generate_Parentheses.c
@@ -0,0 +1,16 @@
+/**
+    * File Name: 22_Generate_Parentheses.c
+    * Author: Louis Xu
+    * Mail: louis_xu_ustc@163.com
+    * Created Time: 03/22/16
+*/
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * For example, given n = 3, a solution set is:
+ *
+ * "((()))", "(()())", "(())()", "()(())", "()()()"
+*/
+
+
