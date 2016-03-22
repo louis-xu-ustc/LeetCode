3f583e9b10502ab47e0730809606ab07522bbea4
louis <345722430@qq.com>
2016-03-22 23:51:09 +0800

    [LeetCode - Q22] update generate parentheses

--- 22_Generate_Parentheses.c
+++ 22_Generate_Parentheses.c
@@ -13,4 +13,54 @@
  * "((()))", "(()())", "(())()", "()(())", "()()()"
 */
 
+/**
+ *  * Return an array of size *returnSize.
+ *  * Note: The returned array must be malloced, assume caller calls free().
+ *  */
 
+void traverse(char *result, int *returnSize, int top, int left, int right, char **array)
+{
+    char *res;
+
+    if (!left && !right) {
+        res = (char *)malloc(sizeof(char) * (top + 2));
+        result[top+1] = '\0';
+        strcpy(res, result);
+        array[*returnSize] = res;
+        (*returnSize)++;
+        return;
+    }
+    if (left) {
+        result[top+1] = '(';
+        traverse(result, returnSize, top+1, left-1, right+1, array);
+    }
+    if (right) {
+        result[top+1] = ')';
+        traverse(result, returnSize, top+1, left, right-1, array);
+    }
+    return;
+}
+
+
+char** generateParenthesis(int n, int* returnSize) {
+    int len = 2*n+1;
+    char result[len];
+    char **array;
+    int size = 0, arraySize = 32768;
+    int left, right, top = -1;
+
+    if (!n) 
+        return NULL;
+
+    array = (char **)malloc(sizeof(char *) * arraySize);
+    memset(array, 0, arraySize);
+
+    left = n;
+    right = 0;
+    *returnSize = 0;
+    traverse(result, &size, top, left, right, array);
+
+    *returnSize = size;
+
+    return array;
+}
